9758a70b761aec591a649318aa288c4572d79d6f
BetNares <btari.f.r@mail.ugm.ac.id>
2023-12-11 18:04:30 +0700

    feat(): add ending screen

---
+++ Assets/Scripts/ExittoMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExittoMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A)) {
+            GoToMainMenu();
+        }
+    }
+    private void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
